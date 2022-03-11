80db30f07dc0540aeda8199a6884d7faf9ed3fcf
vaha77 <abdulvohid996@gmail.com>
2022-03-11 20:48:16 +0900

    new

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import  styled from "styled-components"
-import Navbar from './Components/navbar'
-import Signin from './Components/Signin'
+import React from "react";
+import styled from "styled-components";
+import Navbar from "./Components/navbar";
+import Signin from "./Components/Signin";
 export const App = () => {
   return (
     <AppBox>
-      <Navbar/>
-<Signin/>
+      <Navbar />
+      <Signin />
+      <h1 className="bg-black">ss</h1>
     </AppBox>
-  )
-}
+  );
+};
 
-
-const AppBox =styled.div`
-background: #E5E5E5;
-width: auto;
-height: 100vh;
-
-`
-export default App
+const AppBox = styled.div`
+  background: #e5e5e5;
+  width: auto;
+  height: 100vh;
+`;
+export default App;
---
+++ src/Components/Drawer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Drawer = () => {
+  return (
+    <div>Drawer</div>
+  )
+}
+export default Drawer
