cb99df81b181868bc493ea5599023ee9a7ca19f3
Vaha77 <abdulvohid996@gmail.com>
2022-03-18 21:57:09 +0900

    ls

--- src/pages/MemberDetails/MemberDetails.jsx
+++ src/pages/MemberDetails/MemberDetails.jsx
@@ -111,36 +111,19 @@ color: #FFFFFF;
 
 }
 `
-
 const Container = styled.div`
-
   display: flex;
   flex-direction: column;
 `;
-// const Wrapper = styled.div`
+
+// const Card = styled.div`
 //   display: flex;
 //   flex-direction: column;
-//   background: #ffffff;
-//   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
-//   border-radius: 12px;
-//   margin: 44px;
+//   justify-content: center;
+//   align-items: center;
+//   width: 1526px;
+//   height: 734px;
 // `;
-const Card = styled.div`
-padding: 50px;
-display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 1526px;
-  height: 734px;`
 
-// const Link = styled(NavLink)`
-// text-decoration: none;
-// margin: 62px 0 48px 73px;
-// a{
-//   color: #000000;
-//   margin-right: 43px;
-// }
 
-// `
-export default MemberDetails
+export default MemberDetails;
